6e2d7bd7a6430e7e6c558e90f0c7f0b3be304e15
Айбек Жаксыбаев <futuramacoder@gmail.com>
2021-07-22 18:56:46 +0600

    fix: routing fixed

--- src/uniswap/uniswap.controller.ts
+++ src/uniswap/uniswap.controller.ts
@@ -6,6 +6,12 @@ import { UniswapService } from './uniswap.service';
 export class UniswapController {
     constructor(private readonly uniswapService: UniswapService) {}
 
+    @Get('/:network/staked/info/:accountAddress')
+    public async stakedInfo(@Param('network') network: string, @Param('accountAddress') accountAddress: string, @Res() response: Response) {
+        const result = await this.uniswapService.stakedInfo(network, accountAddress)
+        return response.send(result).status(200)
+    }
+    
     @Get('/:network/transaction/:transactionHash')
     public async getTransaction(@Param('network') network: string, @Param('transactionHash') transactionHash: string, @Res() response: Response) {
         await this.uniswapService.getTransaction(network, transactionHash);
@@ -21,12 +27,6 @@ export class UniswapController {
     @Get('/:network/info/:token0/:token1/:accountAddress')
     public async userInfo(@Param('network') network: string, @Param('token0') token0: string, @Param('token1') token1: string, @Param('accountAddress') accountAddress: string, @Res() response: Response) {
         const result = await this.uniswapService.userInfo(network, token0, token1, accountAddress)
-        return response.send(result).status(200)
-    }
-
-    @Get('/:network/staked/info/:accountAddress')
-    public async stakedInfo(@Param('network') network: string, @Param('accountAddress') accountAddress: string, @Res() response: Response) {
-        const result = await this.uniswapService.stakedInfo(network, accountAddress)
         return response.send(result).status(200)
     }
 }
