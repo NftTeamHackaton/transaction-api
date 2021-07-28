737bcb2cecc3e54e16e0bb63f57ac6600c37e77b
Айбек Жаксыбаев <futuramacoder@gmail.com>
2021-07-28 16:42:55 +0600

    fix: transaction api fixed

--- src/transaction/dto/aaveTx.dto.ts
+++ src/transaction/dto/aaveTx.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class AaveTxDto {
+    @IsNotEmpty()
+    @IsString()
     public erc20Symbol: string;
+
+    @IsNotEmpty()
+    @IsString()
     public address: string;
+
+    @IsNotEmpty()
+    @IsString()
     public operation: string;
 }
--- src/transaction/dto/pancakeSwapTx.dto.ts
+++ src/transaction/dto/pancakeSwapTx.dto.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class PancakeSwapDto {
+    @IsNotEmpty()
+    @IsString()
     public address: string;
+
+    @IsNotEmpty()
+    @IsString()
     public token0: string;
+
+    @IsNotEmpty()
+    @IsString()
     public token1: string;
+
+    @IsNotEmpty()
+    @IsString()
     public operation: string;
 }
