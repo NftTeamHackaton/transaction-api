456034529042c0d2be54921b22aec31573602228
Айбек Жаксыбаев <futuramacoder@gmail.com>
2021-08-05 14:50:43 +0600

    feat: fetch tx changed

--- src/transaction/ethereumTransaction.service.ts
+++ src/transaction/ethereumTransaction.service.ts
@@ -231,33 +231,16 @@ export class EthereumTransactionService {
 
     private async fetchUniswapTransaction(token0Symbol: string, token1Symbol: string, address: string) {
         const pair = `${token0Symbol}-${token1Symbol}`
-        const service = 'uniswap'
-        if(token0Symbol == 'WETH') {
-            token0Symbol = 'ETH'
-        }
-
-        if(token1Symbol == 'WETH') {
-            token1Symbol = 'ETH'
-        }
 
         return this.erc20TransactionRepository.find({order: {nonce: 'DESC'}, where: [
-            {service, pair, tokenSymbol: token0Symbol, from: address, to: '0xcc1b14a6cEF311050eb3A8690F871F98d1F7c4B7'.toLowerCase()},
-            {service, pair, tokenSymbol: token0Symbol, from: '0xcc1b14a6cEF311050eb3A8690F871F98d1F7c4B7'.toLowerCase(), to: address},
+            {pair, from: address, to: '0xcc1b14a6cEF311050eb3A8690F871F98d1F7c4B7'.toLowerCase()},
+            {pair, from: '0xcc1b14a6cEF311050eb3A8690F871F98d1F7c4B7'.toLowerCase(), to: address},
 
-            {service, pair, tokenSymbol: token1Symbol, from: address, to: '0xcc1b14a6cEF311050eb3A8690F871F98d1F7c4B7'.toLowerCase()},
-            {service, pair, tokenSymbol: token1Symbol, from: '0xcc1b14a6cEF311050eb3A8690F871F98d1F7c4B7'.toLowerCase(), to: address},
+            {pair, from: address, to: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'.toLowerCase()},
+            {pair, from: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'.toLowerCase(), to: address},
 
-            {service, pair, tokenSymbol: token0Symbol, from: address, to: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'.toLowerCase()},
-            {service, pair, tokenSymbol: token0Symbol, from: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'.toLowerCase(), to: address},
-
-            {service, pair, tokenSymbol: token1Symbol, from: address, to: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'.toLowerCase()},
-            {service, pair, tokenSymbol: token1Symbol, from: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'.toLowerCase(), to: address},
-
-            {service, pair, tokenSymbol: token1Symbol, from: address, to: '0xd1106dB81792a47DBb702Ad714ca63eF83463309'.toLowerCase()},
-            {service, pair, tokenSymbol: token1Symbol, from: '0xd1106dB81792a47DBb702Ad714ca63eF83463309'.toLowerCase(), to: address},
-
-            {service, pair, tokenSymbol: token0Symbol, from: address, to: '0xd1106dB81792a47DBb702Ad714ca63eF83463309'.toLowerCase()},
-            {service, pair, tokenSymbol: token0Symbol, from: '0xd1106dB81792a47DBb702Ad714ca63eF83463309'.toLowerCase(), to: address},
+            {pair, from: address, to: '0xd1106dB81792a47DBb702Ad714ca63eF83463309'.toLowerCase()},
+            {pair, from: '0xd1106dB81792a47DBb702Ad714ca63eF83463309'.toLowerCase(), to: address},
         ]})
     }
 
