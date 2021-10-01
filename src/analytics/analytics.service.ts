0326119528e0e797660bc5ac1d49f674f810f2c5
Aibek <futuramacoder@gmail.com>
2021-10-01 13:58:17 +0600

    fix: analytics fixed

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -21,8 +21,9 @@ export class AnalyticsService {
     public async operationAnalytics(address: string) {
         const staking = await this.staking(address)
         const lp = await this.lp(address)
+        const swap = await this.swap(address)
         const wallet = await this.wallet(address)
-        return {staking, lp, wallet}
+        return {staking, lp, swap, wallet}
     }
 
     private async wallet(address: string) {
@@ -49,6 +50,10 @@ export class AnalyticsService {
                 data: []
             }
             for(let i = 0; i < walletOperations.length; i++) {
+                if(this.even(i)) {
+                    await this.delay(2000)
+                }
+
                 const walletOperation = walletOperations[i]
                 const value = walletOperation.sum
                 const decimals = walletOperation.tokenDecimals
@@ -82,7 +87,90 @@ export class AnalyticsService {
             throw new BadRequestException(message)
         }
     } 
+    // swap: {
+    //     services: [
+    //         {
+    //             name: 'uniswap',
+    //             logo: "asdasd",
+    //             description: "asdasd",
+    //             usd: 0,
+    //             data: []
+    //         }
+    //     ]
+    // }
+    private async swap(address: string) {
+        try {
+            const uniswapOperations = await this.erc20TransactionRepository.createQueryBuilder('q')
+                .select('q.tokenSymbol', 'tokenSymbol')
+                .addSelect('q.service', 'service')
+                .addSelect('q.tokenDecimals', 'tokenDecimals')
+                .addSelect('q.operation', 'operation')
+                .addSelect('sum(q.value::numeric)', 'sum')
+                .where('(q.operation in (:...operations)) and (q.service in (:...service)) and q.from = :from', {
+                    operations: ['swap'], from: address, service: ['uniswap']
+                })
+                .orWhere('(q.operation in (:...operations)) and (q.service in (:...service)) and q.to = :to', {
+                    operations: ['swap'], to: address, service: ['uniswap']
+                })
+                .groupBy('q.tokenSymbol')
+                .addGroupBy('q.service')
+                .addGroupBy('q.tokenDecimals')
+                .addGroupBy('q.operation')
+                .getRawMany()
+            const swapData = {
+                usd: 0,
+                services: [
+                    {
+                        name: 'uniswap',
+                        logo: "asdasd",
+                        description: "asdasd",
+                        usd: 0,
+                        data: []
+                    }
+                ],
+            }
+            for(let i = 0; i < uniswapOperations.length; i++) {
+                if(this.even(i)) {
+                    await this.delay(2000)
+                }
+                const uniswap = uniswapOperations[i]
+                const value = uniswap.sum
+                const decimals = uniswap.tokenDecimals
+                const divisor = Math.pow(10, decimals) // Web3.utils.toBN(10).pow(decimals)
+                console.log(divisor.toString())
+                const price = value / divisor
+                let usdPrice = 0
+                
+                if(price > 0) {
+                    const response = await this.httpService.get(`/v1/tools/price-conversion?amount=${price}&symbol=${uniswap.tokenSymbol}`).toPromise()
+                    usdPrice = response.data.data.quote.USD.price
+                }
+                console.log(value.toString())
+                const data = {
+                    symbol: uniswap.tokenSymbol,
+                    decimals: uniswap.tokenDecimals,
+                    price: price.toString(),
+                    usd: usdPrice
+                }
 
+                swapData.usd += usdPrice
+
+                if(uniswap.operation == 'swap') {
+                    swapData.services[0].usd += usdPrice
+                    swapData.services[0].data.push(data)
+                }
+            }
+            return swapData;
+        } catch (e) {
+            let message = e.message
+
+            if(e.response) {
+                message = e.response.data
+            }
+
+            throw new BadRequestException(message)
+        }
+    }
     private async lp(address: string) {
         try {
             const uniswapOperations = await this.erc20TransactionRepository.createQueryBuilder('q')
@@ -92,10 +180,10 @@ export class AnalyticsService {
                 .addSelect('q.operation', 'operation')
                 .addSelect('sum(q.value::numeric)', 'sum')
                 .where('(q.operation in (:...operations)) and (q.service in (:...service)) and q.from = :from', {
-                    operations: ['swap', 'addLiquidity'], from: address, service: ['uniswap']
+                    operations: ['addLiquidity'], from: address, service: ['uniswap']
                 })
                 .orWhere('(q.operation in (:...operations)) and (q.service in (:...service)) and q.to = :to', {
-                    operations: ['swap', 'addLiquidity'], to: address, service: ['uniswap']
+                    operations: ['addLiquidity'], to: address, service: ['uniswap']
                 })
                 .groupBy('q.tokenSymbol')
                 .addGroupBy('q.service')
@@ -104,18 +192,20 @@ export class AnalyticsService {
                 .getRawMany()
             const lpData = {
                 usd: 0,
-                uniswap: {
-                    lp: {
-                        usd: 0,
-                        data: []
-                    },
-                    swap: {
+                services: [
+                    {
+                        name: 'uniswap',
+                        logo: "asdasd",
+                        description: "asdasd",
                         usd: 0,
                         data: []
                     }
-                }
+                ],
             }
             for(let i = 0; i < uniswapOperations.length; i++) {
+                if(this.even(i)) {
+                    await this.delay(2000)
+                }
                 const uniswap = uniswapOperations[i]
                 const value = uniswap.sum
                 const decimals = uniswap.tokenDecimals
@@ -139,13 +229,8 @@ export class AnalyticsService {
                 lpData.usd += usdPrice
 
                 if(uniswap.operation == 'addLiquidity') {
-                    lpData.uniswap.lp.usd += usdPrice
-                    lpData.uniswap.lp.data.push(data)
-                }
-
-                if(uniswap.operation == 'swap') {
-                    lpData.uniswap.swap.usd += usdPrice
-                    lpData.uniswap.swap.data.push(data)
+                    lpData.services[0].usd += usdPrice
+                    lpData.services[0].data.push(data)
                 }
             }
             return lpData;
@@ -175,16 +260,27 @@ export class AnalyticsService {
                 .getRawMany()
             const stakingData = {
                 usd: 0,
-                compound: {
-                    usd: 0,
-                    data: []
-                },
-                aave: {
-                    usd: 0,
-                    data: []
-                }
+                services: [
+                    {
+                        name: 'compound',
+                        logo: "asdasd",
+                        description: "asdasd",
+                        usd: 0,
+                        data: []
+                    },
+                    {
+                        name: 'aave',
+                        logo: "asdasd",
+                        description: "asdasd",
+                        usd: 0,
+                        data: []
+                    }
+                ],
             }
             for(let i = 0; i < stakingOperations.length; i++) {
+                if(this.even(i)) {
+                    await this.delay(2000)
+                }
                 const staking = stakingOperations[i]
                 const value = staking.sum
                 const decimals = staking.tokenDecimals
@@ -207,13 +303,13 @@ export class AnalyticsService {
                 stakingData.usd += usdPrice
 
                 if(staking.service == 'compound') {
-                    stakingData.compound.usd += usdPrice
-                    stakingData.compound.data.push(data)
+                    stakingData.services[0].usd += usdPrice
+                    stakingData.services[0].data.push(data)
                 }
 
                 if(staking.service == 'aave') {
-                    stakingData.aave.usd += usdPrice
-                    stakingData.aave.data.push(data)
+                    stakingData.services[1].usd += usdPrice
+                    stakingData.services[1].data.push(data)
                 }
             }
             return stakingData;
@@ -228,4 +324,12 @@ export class AnalyticsService {
         }
     }
 
+    private even(n: number): boolean {
+        return !(n % 2)
+    }
+
+
+    private async delay(second) {
+        return new Promise(res => setTimeout(res, second));
+    }
 }
